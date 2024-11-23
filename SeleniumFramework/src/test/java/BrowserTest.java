bca1f8ba14456b05cd060420c445e235262599a6
AFSALU RAHMAN C <afsalu2002@gmail.com>
2024-11-23 15:04:30 +0530

    2nd commit added geckodriver, path is updated

--- SeleniumFramework/src/test/java/BrowserTest.java
+++ SeleniumFramework/src/test/java/BrowserTest.java
@@ -3,6 +3,10 @@ import org.openqa.selenium.firefox.*;
 public class BrowserTest {
 	public static void main(String[] args) {
 		
+		String projectPath = System.getProperty("user.dir");
+		System.out.println("Project path is : "+projectPath);
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\afsal\\git\\Selenium-Java-Framework\\SeleniumFramework\\Drivers\\Gecko Driver\\geckodriver.exe");
+		System.setProperty("webdriver.firefox.marionette", projectPath+"\\Drivers\\Gecko Driver\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://feelfreetolearn.videocrypt.in/");
 	
