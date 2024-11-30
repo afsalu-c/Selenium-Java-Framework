59a87e5fd574c66ea943319820367772e3a12782
AFSALU RAHMAN C <afsalu2002@gmail.com>
2024-11-30 10:47:58 +0530

    Chrome driver is added, and set a 3 second timing for browser

--- SeleniumFramework/src/test/java/BrowserTest.java
+++ SeleniumFramework/src/test/java/BrowserTest.java
@@ -1,14 +1,26 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.*;
 public class BrowserTest {
 	public static void main(String[] args) {
 		
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("Project path is : "+projectPath);
+		
 		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\afsal\\git\\Selenium-Java-Framework\\SeleniumFramework\\Drivers\\Gecko Driver\\geckodriver.exe");
 		System.setProperty("webdriver.firefox.marionette", projectPath+"\\Drivers\\Gecko Driver\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://feelfreetolearn.videocrypt.in/");
+		//WebDriver driver = new FirefoxDriver();
+		
+		System.setProperty("webdriver.chrome.driver", projectPath+"/Drivers/Chrome driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://github.com/afsalu-c");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.close();
 	
 	}
 }
