44fa4e89777690d68167a5b7479017de85425b32
AFSALU RAHMAN C <afsalu2002@gmail.com>
2024-12-04 17:51:01 +0530

    Webelement elemnt identification and weblocator test using id and x path in textBoxTest.java

--- SeleniumFramework/src/test/java/textBoxTest.java
+++ SeleniumFramework/src/test/java/textBoxTest.java
@@ -3,29 +3,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.*;
-public class BrowserTest {
+public class textBoxTest {
 	public static void main(String[] args) {
 		
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("Project path is : "+projectPath);
 		
 		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\afsal\\git\\Selenium-Java-Framework\\SeleniumFramework\\Drivers\\Gecko Driver\\geckodriver.exe");
-		System.setProperty("webdriver.firefox.marionette", projectPath+"\\Drivers\\Gecko Driver\\geckodriver.exe");//added in environment variable
-		//WebDriver driver = new FirefoxDriver();
+//		System.setProperty("webdriver.firefox.marionette", projectPath+"\\Drivers\\Gecko Driver\\geckodriver.exe");//added in environment variable
+//		WebDriver driver = new FirefoxDriver();
 		
-		System.setProperty("webdriver.chrome.driver", projectPath+"/Drivers/Chrome driver/chromedriver.exe");//added in environment variable
+//		System.setProperty("webdriver.chrome.driver", projectPath+"/Drivers/Chrome driver/chromedriver.exe");//added in environment variable
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		
-		WebElement textbox = driver.findElement(By.id("APjFqb"));
+		//WebElement textbox = driver.findElement(By.id("APjFqb")); //with id
+		
+		
+		WebElement textbox = driver.findElement(By.xpath("//textarea[@name='q']"));//with xpath
 		textbox.sendKeys("Automation step by step");
-//		try {
-//			Thread.sleep(3000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		driver.close();
+		
+
 	
 	}
 }
